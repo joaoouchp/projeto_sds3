feec7b097f10f47a26a7f991826ba9d2dfe9bc58
Joao Pedro <jpkook2011@gmail.com>
2021-09-09 03:46:06 -0300

    4º - Adicionando componentes estáticos

---
+++ frontend/src/components/footer/index.tsx
@@ -0,0 +1,27 @@
+function Footer() {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">
+          App desenvolvido por{" "}
+          <a
+            href="https://github.com/joaoouchp"
+            target="_blank"
+            rel="noreferrer"
+          >
+            João Pedro Dias Caparroz
+          </a>
+        </p>
+        <p className="text-light">
+          <small>
+            <strong>Semana Spring React</strong>
+            <br />
+            Seguindo o conteudo de DevSuperior{" "}
+          </small>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
