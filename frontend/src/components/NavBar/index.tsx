fc2745a97407933603a41136a2cfe0de3da9bfaf
Joao Pedro <jpkook2011@gmail.com>
2021-09-12 13:54:05 -0300

    13º Navegação Home

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,11 +1,14 @@
-import logo from 'assets/img/logo.svg';
+import logo from "assets/img/logo.svg";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
       <div className="container">
         <nav className="my-2 my-md-0 mr-md-3">
-          <img src={logo} alt="CHP" width="120" />
+          <Link to="/">
+            <img src={logo} alt="CHP" width="120" />
+          </Link>
         </nav>
       </div>
     </div>
